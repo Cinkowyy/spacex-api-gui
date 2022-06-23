a845a73fdb5e1c325a84a60ec8fd7d2d86a41927
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-06-23 13:03:09 +0200

    Changes pagination styles

--- scripts/pagination.js
+++ scripts/pagination.js
@@ -1,39 +1,46 @@
 export default class Pagination {
-    constructor(data) {
-        this.data = this.getSlicedData(data);
-        this.currentPage = 1;
-        this.pages = this.data.length;
-        this.headerElement
-        this.header;
-        this.container;
+  constructor(data) {
+    this.data = this.getSlicedData(data);
+    this.currentPage = 1;
+    this.pages = this.data.length;
+    this.headerElement;
+    this.header;
+    this.container;
+  }
+
+  renderPage() {
+    const pageArray = this.data[this.currentPage - 1];
+
+    this.container.innerHTML = "";
+    this.container.appendChild(this.header);
+
+    if (pageArray[0].engineInfo) {
+      let rowWrapper = document.createElement("div");
+      rowWrapper.classList.add("rockets-list-wrapper");
+      pageArray.forEach((el) => {
+        rowWrapper.appendChild(el.render());
+      });
+
+      this.container.appendChild(rowWrapper);
+      return;
     }
 
-    renderPage() {
-        const pageArray = this.data[this.currentPage-1];
+    pageArray.forEach((el) => {
+      this.container.appendChild(el.render());
+    });
+  }
 
-        this.container.innerHTML = '';
-        this.container.appendChild(this.header);
+  getPagination() {
+    let paginationContainer = document.createElement("div");
+    paginationContainer.classList.add("pagination");
 
-        if(pageArray[0].engineInfo) {
-            let rowWrapper = document.createElement('div');
-            rowWrapper.classList.add("rockets-list-wrapper")
-            pageArray.forEach(el => {
-                rowWrapper.appendChild(el.render());
-            })
+    if (this.pages == 1) {
+      paginationContainer.innerHTML = `
+            <div class="active"><span>1</span></div>`;
 
-            this.container.appendChild(rowWrapper);
-            return;
-        } 
-
-        pageArray.forEach(el => {
-            this.container.appendChild(el.render());
-        })
+      return paginationContainer;
     }
-
-    getPagination() {
-        let paginationContainer = document.createElement("div");
-        paginationContainer.classList.add('pagination');
-        paginationContainer.innerHTML = `
+    paginationContainer.innerHTML = `
             <img src="images/arrow-left.svg" alt="prev">
             <div class="active"><span>1</span></div>
             <div><span>2</span></div>
@@ -42,14 +49,14 @@ export default class Pagination {
             <div><span>13</span></div>
             <img src="images/arow-right.svg" alt="next"> 
         `;
-        return paginationContainer;
-    }
+    return paginationContainer;
+  }
 
-    getSlicedData(data) {
-        let slicedArray = [];
-        for(let i = 0; i< data.length; i+=10) {
-            slicedArray.push(data.slice(i, i+10));
-        }
-        return slicedArray;
+  getSlicedData(data) {
+    let slicedArray = [];
+    for (let i = 0; i < data.length; i += 10) {
+      slicedArray.push(data.slice(i, i + 10));
     }
+    return slicedArray;
+  }
 }
